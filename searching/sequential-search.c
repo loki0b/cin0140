d90a18b4a4f933f6adfadde1d20f4f3e5f3c4c61
loki0b <social@henriquegarcialeite.com>
2025-09-16 20:41:47 -0300

    update sequential searching

--- searching/sequential-search.c
+++ searching/sequential-search.c
@@ -1,33 +1,20 @@
-#include <stdio.h>
-
-void swap(int* ap, int* bp);
-void selection_sort(int* arr, int n);
-
-int main() {
-  int arr[] = {53, 124, 5312, 543, 4321, 67, 7, 2};
-  size_t n = sizeof(arr) / sizeof(arr[0]);
-
-  selection_sort(arr, n);
-  for (int i = 0; i < n; i++) printf("%i ", arr[i]);
-  putchar('\n');
-
-  return 0;
-}
-
-void selection_sort(int* arr, int n) {
-  for (int i = 0; i < n - 1; i++) {
-    int min = i;
-    for (int j = i + 1; j < n; j++) {
-      if (arr[j] < arr[min]) min = j;
-    }
-    swap(&arr[i], &arr[min]);
+int sequential_search(int* array, int k, size_t n) {
+  int i = 0;
+  while (i < n) {
+    if (array[i] == k) return i;
+    i++;
   }
+
+  return -1;
 }
 
-void swap(int* ap, int* bp) {
-  int temp;
+// Array need to be sorted
+int improved_sequential_search(int* array, int k, size_t n) {
+  int i = 0;
+  while (i < n && array[i] <= k) {
+    if (array[i] == k) return i;
+    i++;
+  }
 
-  temp = *ap;
-  *ap = *bp;
-  *bp = temp;
+  return -1;
 }
