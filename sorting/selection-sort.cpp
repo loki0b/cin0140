4402c892d9037f4a42aa5e89b82955a17de7cd70
loki0b <contato@henriquegarcialeite.com>
2025-11-14 12:35:58 -0300

    update selection sort

---
+++ sorting/selection-sort.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using std::vector, std::swap;
+
+void selection_sort(vector<int>& arr) {
+  int n = arr.size();
+
+  for (int i = 0; i < n - 1; i++) {
+    int min = i;
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min]) min = j;
+    }
+    swap(arr[i], arr[min]);
+  }
+}
