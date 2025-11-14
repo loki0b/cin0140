2499882df843903523065a3237aea20b49bd6ffa
loki0b <contato@henriquegarcialeite.com>
2025-11-14 08:08:11 -0300

    update bubble sort

--- sorting/bubble-sort.cpp
+++ sorting/bubble-sort.cpp
@@ -1,21 +1,13 @@
 #include <vector>
 
-using std::vector;
+using std::vector, std::swap;
 
 void bubble_sort(vector<int>& arr) {
   int n = arr.size();
 
   for (int i = 0; i < n - 1; i++) {
     for (int j = 0; j < n - i - 1; j++) {
-      if (arr[j + 1] < arr[j]) swap(&arr[j], &arr[j + 1]);
+      if (arr[j + 1] < arr[j]) swap(arr[j], arr[j + 1]);
     }
   }
-}
-
-void swap(int* ap, int* bp) {
-  int temp;
-
-  temp = *ap;
-  *ap = *bp;
-  *bp = temp;
 }
