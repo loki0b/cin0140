07854334a34f3afc8edaa2be138c307c87679ec1
loki0b <contato@henriquegarcialeite.com>
2025-11-25 08:32:17 -0300

    updating floyd warshal: implementing the distance matrix to receive a adjacency list as argument

--- graphs/floyd-warshall.cpp
+++ graphs/floyd-warshall.cpp
@@ -3,17 +3,32 @@
 
 #define LL_MAX numeric_limits<long long>::max()
 
-using std::vector, std::numeric_limits;
-using ll = long long;
+using std::vector, std::pair, std::numeric_limits;
 
-void floyd_warshall(vector<vector<long long>>& graph) {
+void floyd_warshall(vector<vector<pair<int, int>>>& graph, vector<vector<long long>>& distance) {
     int n = graph.size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) distance[i][j] = 0;
+            else distance[i][j] = LL_MAX;
+        }
+    }
+
+    for (int v = 0; v < n; v++) {
+        for (auto& e : graph[v]) {
+            int w = e.first;
+            int weight_v_w = e.second;
+
+            distance[v][w] = weight_v_w;
+        }
+    }
 
     for (int k = 0; k < n; k++) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (graph[i][k] != LL_MAX && graph[k][j] != LL_MAX && graph[i][j] > graph[i][k] + graph[k][j])
-                    graph[i][j] = graph[i][k] + graph[k][j];
+                if (distance[i][k] != LL_MAX && distance[k][j] != LL_MAX && distance[i][j] > distance[i][k] + distance[k][j])
+                    distance[i][j] = distance[i][k] + distance[k][j];
             }
         }
     }
